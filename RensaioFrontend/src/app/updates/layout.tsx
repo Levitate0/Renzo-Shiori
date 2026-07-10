cd6fcea93d0f1d0b45db66ad0cbafdfc1077e2c1
root <root@localhost.localdomain>
2026-07-09 23:55:22 -0600

    Fix Updates page missing app chrome: add PageLayout route layout

---
+++ RensaioFrontend/src/app/updates/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import { PageLayout } from "@/components/comp/layout/page-layout";
+
+export default function UpdatesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <PageLayout mainClassName="p-4 pb-16 sm:px-6 sm:py-4 sm:pb-4 overflow-y-auto">{children}</PageLayout>
+  );
+}
